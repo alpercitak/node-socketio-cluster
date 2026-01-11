c6782b47d6d1c2681e9a1929e9701afa55b21ece
Alper Çıtak <citak.alper@gmail.com>
2026-01-11 13:52:41 +0100

    formatting

--- client/src/components/HelloWorld.vue
+++ client/src/components/HelloWorld.vue
@@ -6,25 +6,25 @@
 </template>
 
 <script setup>
-import { ref } from "vue";
-import { io } from "socket.io-client";
+import { ref } from 'vue';
+import { io } from 'socket.io-client';
 
 defineProps({
   message: { type: String, default: null },
 });
 
-const processId = ref("");
+const processId = ref('');
 const isConnectionReady = ref(false);
 
-const socket = io("ws://localhost:3000/", {
+const socket = io('ws://localhost:3000/', {
   withCredentials: true,
 });
 
-socket.on("connect", () => {
+socket.on('connect', () => {
   isConnectionReady.value = true;
 });
 
-socket.on("processId", (pid) => {
+socket.on('processId', (pid) => {
   processId.value = pid;
 });
 </script>
